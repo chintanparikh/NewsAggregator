e04d8891e95c0d6400c8dcd4486473c19612ec32
chintanparikh <chintanparikh@gatech.edu>
2014-01-07 20:28:43 -0500

    Add after route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,10 @@
 class ArticlesController < ApplicationController
   respond_to :json, :xml
-  
   def index
-    @articles = Article.original
+    @articles = Article.original.last(100)
+  end
+
+  def after
+    #params[:id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ArticleAggregator::Application.routes.draw do
   resources :articles
+  get '/articles/after/:id', to: 'articles#after'
   root :to => 'articles#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
